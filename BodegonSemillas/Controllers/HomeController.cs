f3582ee70bc4e26c13cda83b041af759bb82d7a4
mario <mario@MARIO>
2023-03-15 13:08:46 -0600

    Agregar archivos de proyecto.

---
+++ BodegonSemillas/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using BodegonSemillas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace BodegonSemillas.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        string baseURL = "http://192.168.100.10:5014";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            DataTable dt = new DataTable();
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await client.GetAsync("productos/saludar");
+                
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    dt=JsonConvert.DeserializeObject<DataTable>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error en al api");
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
