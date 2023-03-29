fc216ac1a595d4455076e015b0a366133eddc1d5
MarioDorantes <mario13.medh@gmail.com>
2023-03-29 14:51:57 -0600

    Adecuaciones en el registro usuario, valida campos vacios y contraseñas iguales

---
+++ BodegonSemillas/Controllers/RegistroUsuarioFacturaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BodegonSemillas.Controllers
+{
+    public class RegistroUsuarioFacturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BodegonSemillas/Controllers/TerminosYCondicionesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BodegonSemillas.Controllers
+{
+    public class TerminosYCondicionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
