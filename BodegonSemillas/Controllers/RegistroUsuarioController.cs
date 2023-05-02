c3e987266696adbc081cfa38348ecdf9799a0719
MarioDorantes <mario13.medh@gmail.com>
2023-05-02 15:26:47 -0600

    Cambios en el layout

--- BodegonSemillas/Controllers/RegistroUsuarioController.cs
+++ BodegonSemillas/Controllers/RegistroUsuarioController.cs
@@ -12,7 +12,7 @@ namespace BodegonSemillas.Controllers
         {
             return View();
         }
-
+		
 		[HttpPost]
 		public async Task<IActionResult> RegistrarClienteAsync(Cliente cliente)
 		{
@@ -41,6 +41,42 @@ namespace BodegonSemillas.Controllers
 					else
 					{
 						ViewData["Error"] = "Hubo un problema al registrar el cliente";
+						return View();
+					}
+				}
+				catch (HttpRequestException ex)
+				{
+					Console.WriteLine("La conexión falló" + ex.Message);
+				}
+			}
+
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ValidarEmail([FromBody] string email)
+		{
+			
+			var handler = new HttpClientHandler();
+			handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+			using (var client = new HttpClient(handler))
+			{
+				ServicePointManager.ServerCertificateValidationCallback =
+				(sender, cert, chain, sslPolicyErrors) => true;
+
+				try
+				{
+					var content = new StringContent(email, Encoding.UTF8, "application/json");
+					var response = await client.PostAsync("http://192.168.100.10:5297/cliente/validateaccount", content);
+					if (response.IsSuccessStatusCode)
+					{
+						var result = await response.Content.ReadAsStringAsync();
+						Console.WriteLine(result);
+					}
+					else
+					{
+						ViewData["Error"] = "Error en el servidor";
 						return View();
 					}
 				}
