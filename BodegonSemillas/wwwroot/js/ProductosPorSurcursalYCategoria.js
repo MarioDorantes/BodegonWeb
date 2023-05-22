7f3a3465593c555a748079c46937137219b11fbe
MarioDorantes <mario13.medh@gmail.com>
2023-05-22 14:16:35 -0600

    Se muestran precios

--- BodegonSemillas/wwwroot/js/ProductosPorSurcursalYCategoria.js
+++ BodegonSemillas/wwwroot/js/ProductosPorSurcursalYCategoria.js
@@ -13,7 +13,7 @@ function cargarProductos() {
 
             for (var i = 0; i < data.length; i++) {
                 var producto = data[i];
-                var card = crearCard(producto.nombre);
+                var card = crearCard(producto.nombre, producto.precioImpuestos);
 
                 if (i % 3 === 0) {
                     filaActual = $('<div class="row mb-4"></div>');
@@ -30,14 +30,15 @@ function cargarProductos() {
         });
 }
 
-function crearCard(nombreProducto) {
+function crearCard(nombreProducto, precioImpuestos) {
     var card = $('<div class="card" style="width: 18rem;"></div>');
     var iconoFav = $('<a href="#" class="btn btn-link" style="position: absolute; top: 0; right: 0;"><i class="far fa-heart" style="background-color: white; padding: 4px;"></i></a>');
 
     card.append(iconoFav);
     card.append('<img src="..." class="card-img-top" alt="...">');
     card.append('<div class="card-body">');
-    card.append('<p class="card-text">' + nombreProducto + '</p>');
+    card.append('<p class="card-text" style="margin-bottom: 2px; padding-left: 8px; font-size: 25px;">$' + precioImpuestos + '</p>');
+    card.append('<p class="card-text" style="margin-bottom: 2px; padding-left: 8px;">' + nombreProducto + '</p>');
     card.append('</div>');
 
     return card;
