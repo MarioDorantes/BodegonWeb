2fdead65ab3c6e602c10762b2631d12d2c57d86f
MarioDorantes <mario13.medh@gmail.com>
2023-06-05 01:33:56 -0600

    Obtener direcciones funcionando correctamente

--- BodegonSemillas/Controllers/RealizarPedidoController.cs
+++ BodegonSemillas/Controllers/RealizarPedidoController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BodegonSemillas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace BodegonSemillas.Controllers
 {
     public class RealizarPedidoController : Controller
     {
+
+        string baseClientesURL = "http://192.168.100.10:5297";
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerDirecciones()
+        {
+
+            string cliente = HttpContext.Session.GetString("IdCliente");
+            //Es la sucursal seleccionada, es nombrada como dirección por como se construyó en el servicio API
+            int direccion = 60;
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            using (var client = new HttpClient(handler))
+            {
+                ServicePointManager.ServerCertificateValidationCallback =
+                (sender, cert, chain, sslPolicyErrors) => true;
+
+                client.BaseAddress = new Uri(baseClientesURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("direccion/all?cliente=" + cliente + "&direccion=" + direccion);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        List<VistaDireccion> direcciones = JsonConvert.DeserializeObject<List<VistaDireccion>>(json);
+                        return Json(direcciones);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ocurrió un error, intente más tarde");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine("La conexión falló" + ex.Message);
+                }
+
+            }
+
+            return View();
+        }
+
     }
 }
---
+++ BodegonSemillas/Models/VistaDireccion.cs
@@ -0,0 +1,31 @@
+﻿namespace BodegonSemillas.Models
+{
+    public class VistaDireccion
+    {
+        public int Iddireccion { get; set; }
+
+        public int Idcliente { get; set; }
+
+        public string? NombreCompleto { get; set; }
+
+        public string? Pais { get; set; }
+
+        public string? Estado { get; set; }
+
+        public string? Colonia { get; set; }
+
+        public string? NombreCalle { get; set; }
+
+        public string? Ciudad { get; set; }
+
+        public string? NumeroInt { get; set; }
+
+        public string? NumeroExt { get; set; }
+
+        public string? Cp { get; set; }
+        public ulong? Seleccionada { get; set; }
+
+        public string? NotasDireccion { get; set; }
+        public ulong? EstadoActivo { get; set; }
+    }
+}
